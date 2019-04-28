d65a09141840784a3b7b25eacc6d5e69a4248cc4
David Jetter <davidajetter@gmail.com>
2019-04-28 15:28:24 -0400

    correct main.py to use rules

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 from src.FizzBuzz.FizzBuzzEvaluator import FizzBuzzEvaluator
+from src.FizzBuzz.ZeroRemainderRule import ZeroRemainderRule
 
+rules = [
+    ZeroRemainderRule(3, "Fizz"),
+    ZeroRemainderRule(5, "Buzz"),
+    ZeroRemainderRule(7, "Bang")
+]
 
-evaluator = FizzBuzzEvaluator(3, 5)
+evaluator = FizzBuzzEvaluator(rules)
 
-for value in range(1, 101):
+for value in range(1, 106):
     evaluated = evaluator.evaluate(value)
     print(evaluated)
